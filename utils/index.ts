8acd4253eea35970d33a8c623079ed3639721ba0
RohitKewate <rohit751kewate@gmail.com>
2023-09-28 17:00:03 +0530

    Add existing project files to Git

--- utils/index.ts
+++ utils/index.ts
@@ -7,7 +7,7 @@ export async function fetchCars(filters:FilterProps) {
         'X-RapidAPI-Key': '338d5fef9fmsh160255915baa1e4p18450djsn9c642f0561c5',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fule_type=${fuel}`,{headers:headers})
+    const response = await fetch(`${process.env.CAR_API}?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fule_type=${fuel}`,{headers:headers})
     const results = await response.json();
 
     return results
