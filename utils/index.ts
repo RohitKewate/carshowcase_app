ee993b4236d309c2ef443a9be9e012c34b25fe92
RohitKewate <rohit751kewate@gmail.com>
2023-09-28 17:07:59 +0530

    Add existing project files

--- utils/index.ts
+++ utils/index.ts
@@ -29,10 +29,10 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   };
 
   export const generatCarUrl = (car:CarProps,angle?:string) =>{
-    const url = new URL('https://cdn.imagin.studio/getimage');
+    const url = new URL(`${process.env.CAR_IMAGE_URL}`);
 
     const {make,year,model} = car
-    url.searchParams.append('customer','hrjavascript-mastery');
+    url.searchParams.append('customer',`${process.env.CAR_IMAGE_KEY}`);
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(" ")[0]);
     url.searchParams.append('zoomType', 'fullscreen');
